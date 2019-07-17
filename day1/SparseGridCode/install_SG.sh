70571c094c9bc5e6c6b1b487be631b096e2bb08e
amarlon <marlonazinovic@aol.com>
2019-07-17 15:25:03 +0200

    correction

--- day1/SparseGridCode/install_SG.sh
+++ day1/SparseGridCode/install_SG.sh
@@ -24,6 +24,6 @@ echo " IPOPT and PYIPOPT is installed "
 cd
 pwd=`pwd`
 echo 'module load python' >> ~/.bashrc 
-echo 'export LD_LIBRARY_PATH='$PREFIX'/lib:'$LD_LIBRARY_PATH
-echo 'export IPOPT_DIR="'$pwd'/OSE2019/day1/SparseGridCode/pyipopt_midway/Ipopt-3.12.5/build"'
-echo 'export LD_LIBRARY_PATH='$IPOPT_DIR'/lib:'$LD_LIBRARY_PATH
+echo 'export LD_LIBRARY_PATH='$PREFIX'/lib:'$LD_LIBRARY_PATH >> ~/.bashrc 
+echo 'export IPOPT_DIR="'$pwd'/OSE2019/day1/SparseGridCode/pyipopt_midway/Ipopt-3.12.5/build"' >> ~/.bashrc 
+echo 'export LD_LIBRARY_PATH='$IPOPT_DIR'/lib:'$LD_LIBRARY_PATH >> ~/.bashrc 
