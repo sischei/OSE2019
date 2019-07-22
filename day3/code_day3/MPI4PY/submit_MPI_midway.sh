6685dba07297902948ccd6868cb1609d2ed2f3c4
Simon Scheidegger <simonsch@midway-login2.rcc.local>
2019-07-21 19:35:41 -0500

    updated slurm file

--- day3/code_day3/MPI4PY/submit_MPI_midway.sh
+++ day3/code_day3/MPI4PY/submit_MPI_midway.sh
@@ -5,7 +5,9 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=8
  
-module load mpi4py/1.3+python-2.7-2015q2
+#module load mpi4py/1.3+python-2.7-2015q2
+#mpi4py/1.3+intelmpi-4.0
+module load Anaconda2
 
 mpirun python bcast.py
 
